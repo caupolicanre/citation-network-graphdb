611744023a61a15c212dff992b1b9fc1fd667a84
caupolicanre <caupolicanre@gmail.com>
2024-05-19 20:48:56 -0300

    Create Author model and AuthorOrganizationRel relationship model

---
+++ apps/author/models.py
@@ -0,0 +1,30 @@
+import os
+from os.path import join, dirname
+
+import dotenv
+
+from neo4j import GraphDatabase
+import neomodel
+from neomodel import (
+    config, StructuredNode, StructuredRel,
+    UniqueIdProperty, StringProperty, IntegerProperty, FloatProperty, BooleanProperty,
+    DateProperty, DateTimeProperty, AliasProperty, JSONProperty, ArrayProperty,
+    Relationship, RelationshipTo, RelationshipFrom, One, ZeroOrOne, ZeroOrMore, OneOrMore
+)
+
+
+class AuthorOrganizationRel(StructuredRel):
+    author_org_id = UniqueIdProperty()
+
+    @property
+    def papers(self):
+        pass
+
+
+class Author(StructuredNode):
+    author_id = IntegerProperty(unique_index=True, required=True)
+    name = StringProperty(unique_index=True, required=True)
+    organization = RelationshipTo('apps.institution.models.Organization', 'AFFILIATED_WITH', cardinality=ZeroOrMore, model=AuthorOrganizationRel)
+
+    def __str__(self):
+        return self.name
