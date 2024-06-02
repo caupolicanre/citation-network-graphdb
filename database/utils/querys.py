3f59c20c751f5528fce3fb16c56a9558f323e63c
caupolicanre <caupolicanre@gmail.com>
2024-06-02 11:48:50 -0300

    Refactor count_nodes function to use Enums

--- database/utils/querys.py
+++ database/utils/querys.py
@@ -1,14 +1,18 @@
+from typing import Union
 from neomodel import config, db
 
-def count_nodes(label: str, database_url: str, database_name: str) -> int:
+from core.app_enums import AuthorApp, InstitutionApp, PaperApp
+
+
+def count_nodes(label: Union[AuthorApp, InstitutionApp, PaperApp], database_url: str, database_name: str) -> int:
     '''
     Count the number of nodes with a specific label in the database.
     Using the neomodel library.
 
     Parameters
     ----------
-    label : str
-        Node label.
+    label : Union[AuthorApp, InstitutionApp, PaperApp]
+        Label of the nodes to be counted.
     database_url : str
         URL of the database.
     database_name : str
@@ -22,7 +26,7 @@ def count_nodes(label: str, database_url: str, database_name: str) -> int:
     config.DATABASE_URL = database_url
     config.DATABASE_NAME = database_name
 
-    query = f"MATCH (n:{label}) RETURN count(n) as count"
+    query = f"MATCH (n:{label.value}) RETURN count(n) as count"
     results, meta = db.cypher_query(query)
     
     return results[0][0]
