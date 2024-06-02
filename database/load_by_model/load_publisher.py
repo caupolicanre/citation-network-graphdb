daa8f41b1054cf92a2205dce74186db6acccaefa
caupolicanre <caupolicanre@gmail.com>
2024-06-01 21:40:06 -0300

    Refactor script for loading Publishers to Graph Database

--- database/load_by_model/load_publisher.py
+++ database/load_by_model/load_publisher.py
@@ -11,35 +11,26 @@ from apps.institution.models import Publisher
 
 
 
-dataset_path = './dataset/dblp.v12.json'
-encoding = detect_encoding(dataset_path)
+def create_publisher_nodes(nodes: list, database_url: str, database_name: str) -> None:
+    '''
+    Create nodes for each publisher in the dataset with neomodel.
+    Using the Publisher model.
 
-BATCH_SIZE = 10000
-nodes_batch = []
+    Parameters
+    ----------
+    nodes : list
+        A list of dictionaries containing the publisher's information.
+        Required fields:
+            - publisher: string (required)
+                Publisher name.
+    database_url : str
+        URL of the database.
+    database_name : str
+        Name of the database.
+    '''
+    config.DATABASE_URL = database_url
+    config.DATABASE_NAME = database_name
 
-
-
-print('==============================')
-print(' Populate Database: Publisher')
-print('==============================')
-print('Choose Database:\n1. Production\n2. Test')
-
-db_option = None
-while db_option not in ['Production', 'Test']:
-    db_option = input('\nDatabase: ')
-
-    if db_option not in ['Production', 'Test']:
-        print('Invalid Database. Please choose between \'Production\' and \'Test\'.')
-
-database_url, database_name = neomodel_connect(db_option)
-
-config.DATABASE_URL = database_url
-config.DATABASE_NAME = database_name
-
-
-
-
-def create_nodes_batch(nodes):
     with db.transaction:
         for obj in nodes:
             publisher = obj.get('publisher', None)
@@ -50,24 +41,49 @@ def create_nodes_batch(nodes):
                 if not publisher_node:
                     publisher_node = Publisher(name=publisher).save()
 
-    gc.collect()  # Garbage collection to free up memory
+    gc.collect()
 
 
 
-print('\nLoading Publishers to Graph Database...')
+
+if __name__ == '__main__':
+    dataset_path = './dataset/dblp.v12.json'
+    encoding = detect_encoding(dataset_path)
+
+    BATCH_SIZE = 10000
+    nodes_batch = []
 
 
-with open(dataset_path, 'r', encoding=encoding) as f:
-    objects = ijson.items(f, 'item')
 
-    for obj in tqdm(objects, desc='Creating nodes', unit=' papers'):
-        nodes_batch.append(obj)
-        if len(nodes_batch) >= BATCH_SIZE:
-            create_nodes_batch(nodes_batch)
-            nodes_batch = []
+    print('==============================')
+    print(' Populate Database: Publisher')
+    print('==============================')
+    print('Choose Database:\n1. Production\n2. Test')
 
-    if nodes_batch:
-        create_nodes_batch(nodes_batch)
+    db_option = None
+    while db_option not in ['Production', 'Test']:
+        db_option = input('\nDatabase: ')
+
+        if db_option not in ['Production', 'Test']:
+            print('Invalid Database. Please choose between \'Production\' and \'Test\'.')
+
+    database_url, database_name = neomodel_connect(db_option)
 
 
-print('\nPublishers loaded to Graph Database.')
+
+    print('\nLoading Publishers to Graph Database...')
+
+    with open(dataset_path, 'r', encoding=encoding) as f:
+        objects = ijson.items(f, 'item')
+
+        for obj in tqdm(objects, desc='Creating nodes', unit=' papers'):
+            nodes_batch.append(obj)
+
+            if len(nodes_batch) >= BATCH_SIZE:
+                create_publisher_nodes(nodes_batch, database_url, database_name)
+                nodes_batch = []
+
+        if nodes_batch:
+            create_publisher_nodes(nodes_batch, database_url, database_name)
+
+    print('\nPublishers loaded to Graph Database.')
