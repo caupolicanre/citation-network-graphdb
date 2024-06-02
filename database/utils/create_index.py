7f3713a52d4f322bb90a306418b862be0d644151
caupolicanre <caupolicanre@gmail.com>
2024-06-02 10:59:03 -0300

    Refactor driver connection

--- database/utils/create_index.py
+++ database/utils/create_index.py
@@ -1,3 +1,5 @@
+import gc
+
 import pandas as pd
 
 import neo4j
@@ -7,29 +9,26 @@ from database.utils.db_connection import neo4j_connect
 
 
 
-print('=============================')
-print(' Create Indexes for Database')
-print('=============================')
-print('Choose Database:\n1. Production\n2. Test')
+def create_index(node_label: str, field_name: str, database_url:str, database_name: str, auth: tuple) -> None:
+    '''
+    Create index for a field in a node.
 
-db_option = None
-while db_option not in ['Production', 'Test']:
-    db_option = input('\nDatabase: ')
+    Parameters
+    ----------
+    node_label : str
+        Label of the node.
+    field_name : str
+        Field to create index for.
+    database_url : str
+        URL of the database.
+    database_name : str
+        Name of the database.
+    auth : tuple
+        Authentication tuple. (username, password)
+    '''
+    driver = GraphDatabase.driver(database_url, auth=auth)
 
-    if db_option not in ['Production', 'Test']:
-        print('Invalid Database. Please choose between \'Production\' and \'Test\'.')
-
-database_url, database_name, auth = neo4j_connect(db_option)
-
-print(f'Database: {database_name}')
-
-driver = GraphDatabase.driver(database_url, auth=auth)
-session = driver.session(database=database_name)
-
-
-
-def create_index(node_label: str, field_name: str):
-    with driver.session() as session:
+    with driver.session(database=database_name) as session:
         try:
             print(f'\nCreating index for {node_label}.{field_name}')
             session.run(f"CREATE INDEX FOR (n:{node_label}) ON (n.{field_name})")
@@ -40,48 +39,90 @@ def create_index(node_label: str, field_name: str):
                 print(f'Error: {e}')
             else:
                 raise e
+    
+    session.close()
+    driver.close()
+    gc.collect()
 
 
-def get_indexes() -> pd.DataFrame:
-    with driver.session() as session:
+def get_indexes(database_url: str, database_name: str, auth: tuple) -> pd.DataFrame:
+    '''
+    Get indexes in the database.
+
+    Parameters
+    ----------
+    database_url : str
+        URL of the database.
+    database_name : str
+        Name of the database.
+    auth : tuple
+        Authentication tuple. (username, password)
+    
+    Returns
+    -------
+    df : pd.DataFrame
+        Indexes in the database.
+    '''
+    driver = GraphDatabase.driver(database_url, auth=auth)
+
+    with driver.session(database=database_name) as session:
         result = session.run('SHOW INDEXES')
         indexes = result.data()
         df = pd.DataFrame(indexes)
+    
+    session.close()
+    driver.close()
+    gc.collect()
 
     return df
 
         
 
 
-nodes_to_index = [
-    {'node_label': 'Paper', 'field_name': 'paper_id'},
-    {'node_label': 'Paper', 'field_name': 'title'},
+if __name__ == '__main__':
+    print('=============================')
+    print(' Create Indexes for Database')
+    print('=============================')
+    print('Choose Database:\n1. Production\n2. Test')
 
-    {'node_label': 'Author', 'field_name': 'author_id'},
-    {'node_label': 'Author', 'field_name': 'name'},
+    db_option = None
+    while db_option not in ['Production', 'Test']:
+        db_option = input('\nDatabase: ')
 
-    {'node_label': 'Organization', 'field_name': 'name'},
-    {'node_label': 'Venue', 'field_name': 'name'},
+        if db_option not in ['Production', 'Test']:
+            print('Invalid Database. Please choose between \'Production\' and \'Test\'.')
 
-    {'node_label': 'FieldOfStudy', 'field_name': 'name'}
-]
+    database_url, database_name, auth = neo4j_connect(db_option)
 
-
-print(f'\nCreating indexes for \'{db_option}\' Database...')
-
-for node in nodes_to_index:
-    create_index(node['node_label'], node['field_name'])
-
-print('\nIndexes created successfully.')
+    print(f'Database: {database_name}')
 
 
-show_indexes = input('\nShow indexes? (y/n): ')
 
-if show_indexes.lower() in ['y', 'yes']:
-    indexes = get_indexes()
-    print(f'\nIndexes in \'{db_option}\' Database:')
-    print(indexes)
+    nodes_to_index = [
+        {'node_label': 'Paper', 'field_name': 'paper_id'},
+        {'node_label': 'Paper', 'field_name': 'title'},
+
+        {'node_label': 'Author', 'field_name': 'author_id'},
+        {'node_label': 'Author', 'field_name': 'name'},
+
+        {'node_label': 'Organization', 'field_name': 'name'},
+        {'node_label': 'Venue', 'field_name': 'name'},
+
+        {'node_label': 'FieldOfStudy', 'field_name': 'name'}
+    ]
 
 
-session.close()
-driver.close()
+    print(f'\nCreating indexes for \'{db_option}\' Database...')
+
+    for node in nodes_to_index:
+        create_index(node['node_label'], node['field_name'], database_url, database_name, auth)
+
+    print('\nIndexes created successfully.')
+
+
+    show_indexes = input('\nShow indexes? (y/n): ')
+
+    if show_indexes.lower() in ['y', 'yes']:
+        indexes = get_indexes(database_url, database_name, auth)
+        print(f'\nIndexes in \'{db_option}\' Database:')
+        print(indexes)
