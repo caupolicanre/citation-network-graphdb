e71c1ab4a989cb808ac08d8b47f5780933909e90
caupolicanre <caupolicanre@gmail.com>
2024-06-02 11:58:07 -0300

    Refactor count_nodes function to use Enums, and add option to count all DB nodes

--- database/utils/querys.py
+++ database/utils/querys.py
@@ -1,32 +1,38 @@
-from typing import Union
+from typing import Union, Optional
 from neomodel import config, db
 
 from core.app_enums import AuthorApp, InstitutionApp, PaperApp
 
 
-def count_nodes(label: Union[AuthorApp, InstitutionApp, PaperApp], database_url: str, database_name: str) -> int:
+def count_nodes(database_url: str, database_name: str,
+                label: Optional[Union[AuthorApp, InstitutionApp, PaperApp]] = None) -> int:
     '''
-    Count the number of nodes with a specific label in the database.
+    Count the number of nodes with the specified label in the database.
+    If no label is specified, count all nodes in the database.
     Using the neomodel library.
 
     Parameters
     ----------
-    label : Union[AuthorApp, InstitutionApp, PaperApp]
-        Label of the nodes to be counted.
     database_url : str
         URL of the database.
     database_name : str
         Name of the database.
+    label : Optional[Union[AuthorApp, InstitutionApp, PaperApp]], optional
+        Label of the nodes to be counted, by default None.
     
     Returns
     -------
-    int
-        Number of nodes with the specified label.
+    results[0][0] : int
+        Number of nodes with the specified label in the database.
     '''
     config.DATABASE_URL = database_url
     config.DATABASE_NAME = database_name
 
-    query = f"MATCH (n:{label.value}) RETURN count(n) as count"
+    if label:
+        query = f"MATCH (n:{label.value}) RETURN count(n)"
+    else:
+        query = "MATCH (n) RETURN count(n)"
+    
     results, meta = db.cypher_query(query)
     
     return results[0][0]
