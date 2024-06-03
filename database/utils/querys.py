a03abb865910f270ca452e01c7540fa45d0f0d74
caupolicanre <caupolicanre@gmail.com>
2024-06-03 17:51:33 -0300

    Create count_relationships query

--- database/utils/querys.py
+++ database/utils/querys.py
@@ -36,3 +36,45 @@ def count_nodes(database_url: str, database_name: str,
     results, meta = db.cypher_query(query)
     
     return results[0][0]
+
+
+def count_relationships(database_url: str, database_name: str,
+                        node_a: Optional[Union[AuthorApp, InstitutionApp, PaperApp]] = None,
+                        node_b: Optional[Union[AuthorApp, InstitutionApp, PaperApp]] = None) -> int:
+    '''
+    Count the number of relationships within the specified nodes in the database.
+    If only one node is specified, count all relationships with that node.
+    If no nodes are specified, count all relationships in the database.
+    Using the neomodel library.
+
+    Parameters
+    ----------
+    database_url : str
+        URL of the database.
+    database_name : str
+        Name of the database.
+    node_a : Optional[Union[AuthorApp, InstitutionApp, PaperApp]], optional
+        Label of the first node in the relationship, by default None.
+    node_b : Optional[Union[AuthorApp, InstitutionApp, PaperApp]], optional
+        Label of the second node in the relationship, by default None.
+    
+    Returns
+    -------
+    results[0][0] : int
+        Number of relationships with the specified label in the database.
+    '''
+    config.DATABASE_URL = database_url
+    config.DATABASE_NAME = database_name
+
+    if node_a and node_b:
+        query = f"MATCH (n:{node_a.value})-[r]->(m:{node_b.value}) RETURN count(r)"
+    elif node_a:
+        query = f"MATCH (n:{node_a.value})-[r]->() RETURN count(r)"
+    elif node_b:
+        query = f"MATCH ()-[r]->(n:{node_b.value}) RETURN count(r)"
+    else:
+        query = "MATCH ()-[r]->() RETURN count(r)"
+    
+    results, meta = db.cypher_query(query)
+    
+    return results[0][0]
